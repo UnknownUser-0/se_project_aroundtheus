cf40ae01ee840f211aa6708165837d779ab85f66
Cristian <avilacristian39@gmail.com>
2024-02-21 00:08:49 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,35 @@
-const cardData1 = {
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-};
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    },
 
-const cardData2 = {
-    name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-};
+    {
+        name: "Lake Louise",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    },
 
-const cardData3 = {
-    name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-};
+    {
+        name: "Bald Mountains",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    },
 
-const cardData4 = {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-};
+    {
+        name: "Latemar",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    },
 
-const cardData5 = {
-    name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-};
+    {
+        name: "Vanoise National Park",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    },
 
-const cardData6 = {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-};
+    {
+        name: "Lago di Braies",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    },
 
-const initialCards = [cardData1, cardData2, cardData3, cardData4, cardData5, cardData6];
-console.log(initialCards);
+];
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
@@ -39,9 +39,21 @@ const profileDescription = document.querySelector(".profile__description");
 const profileTitleInput = document.querySelector(".modal__title"); 
 const profileDescriptionInput = document.querySelector(".modal__description");
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
 
 function closePopop() {
     profileEditModal.classList.remove("modal__opened");
+}
+
+function getCardElement(cardData) {
+    const cardElement = cardTemplate.cloneNode(true);
+const cardImageEl = cardElement.querySelector(".card__image");
+const cardTitleEl = cardElement.querySelector(".card__title");
+cardTitleEl.textContent = cardData.name;
+cardElement.alt = cardData.link;
+cardImageEl.src = cardData.link; 
+return cardElement;
 }
 
 function handleProfileEditSubmit(e) {
@@ -61,3 +73,7 @@ profileEditButton.addEventListener("click", () => {
 
 profileCloseButton.addEventListener("click", closePopop);
 
+initialCards.forEach((cardData) => {
+const cardElement = getCardElement(cardData);
+cardListEl.append(cardElement);
+});
